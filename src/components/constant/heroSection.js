5ba9d3fe3da34832c1ac5a404d58c7ec82de9421
LiveWithCodeAnkit <ankityadav119live@gmail.com>
2023-07-27 16:22:56 +0530

    add Hero section UI

--- src/components/constant/heroSection.js
+++ src/components/constant/heroSection.js
@@ -2,19 +2,23 @@ export const heroSectionCards = [
   {
     id: 1,
     title: "Build your form your way",
-    describtion:
+    description:
       "You already know HTML and CSS, so build your form exactly the way you want to. We don't get in your way with JavaScript widgets, WYSIWYG editors, or janky iframes.",
   },
   {
-    id: 1,
+    id: 2,
     title: "Point your form at FieldGoal",
-    describtion:
+    description:
       "Create a new FieldGoal endpoint and we'll give you a unique URL to point your form at. Add it as your form's action attribute and you're all set.",
   },
   {
-    id: 1,
+    id: 3,
     title: "Delivered to where you desire",
-    describtion:
-      "With Zapier integration, you can get your submissions delivered to wherever you desire. Learn more about using Zapier to integrate apps with FieldGoal.",
+    description:
+      "With Zapier integration, you can get your submissions delivered to wherever you desire.",
+    link: {
+      href: "#",
+      text: "Learn more about using Zapier to integrate apps with FieldGoal.",
+    },
   },
 ];
